d67c74fd6e3b9de9d92baa55fe724ca5f8d4f78f
ChrSteinert <chr.steinert@outlook.com>
2018-04-24 18:12:52 +0200

    Adds Array.Copy for SubArray benchmarks.

--- src/SubArrays.fs
+++ src/SubArrays.fs
@@ -14,8 +14,13 @@ type SubArrays () =
     member __.Indexer () =
         array.[5000..5999]
 
+    [<Benchmark>]
+    member __.Copy () =
+        let target : byte array = Array.zeroCreate 1000
+        System.Array.Copy(array, 5000, target, 0, 1000)
+
     [<Benchmark(Baseline = true)>]
-    member __.CopyTo () =
+    member __.ForCopy () =
         let target : byte array = Array.zeroCreate 1000
         for i = 5000 to 5999 do
             target.[i - 5000] = array.[i]
